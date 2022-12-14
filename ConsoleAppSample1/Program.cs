d70e49d4f32232f0954a16df6d8e131372caf758
makoto <ppkotonari@gmail.com>
2022-12-14 11:04:02 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleAppSample1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+
+bool isEsc = false;
+while (!isEsc)
+{
+    Console.WriteLine($"キーを入力してください。");
+    var input = Console.ReadKey(true);
+    Console.WriteLine($"{input.Key} が入力されました。");
+
+    isEsc = input.Key == ConsoleKey.Escape;
+}
+
+Console.WriteLine($"アプリを終了します。");
+Console.ReadKey();
